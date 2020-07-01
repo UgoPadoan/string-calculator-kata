e98992de7434114ece65326d36cabf4fe5a44944
Andrea Cirri <andrea.cirri@injenia.it>
2020-07-01 10:23:17 +0200

    should return sum when delimiter is semicolon implementation

--- kotlin/src/main/kotlin/stringcalculator/StringCalculator.kt
+++ kotlin/src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -2,8 +2,17 @@ package stringcalculator
 
 object StringCalculator {
     fun sum(commaSeparatedNumbers: String): Int {
-        return commaSeparatedNumbers
-                .split("[\n,]".toRegex())
+
+        var delimiter = "\n,"
+        var numbers = commaSeparatedNumbers;
+
+        if (commaSeparatedNumbers.startsWith("//")){
+            delimiter = commaSeparatedNumbers.substring(2, commaSeparatedNumbers.indexOf("\n"))
+            numbers = commaSeparatedNumbers.substringAfter("\n")
+        }
+
+        return numbers
+                .split("[${delimiter}]".toRegex())
                 .filterNot { it.isEmpty() }
                 .sumBy { it.toInt() }
     }
