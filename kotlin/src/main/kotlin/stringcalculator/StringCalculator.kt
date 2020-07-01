a041a09865d7f4b7fb6e13ab6c323afdb158d2ba
Francesco <Francesco>
2020-07-01 09:42:36 +0200

    mob next [ci-skip]

--- kotlin/src/main/kotlin/stringcalculator/StringCalculator.kt
+++ kotlin/src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -2,7 +2,8 @@ package stringcalculator
 
 class StringCalculator {
     fun add(numbers: String): Int {
-        return 0
+        if(numbers.isEmpty()) return 0
+        return numbers.toInt()
     }
 
 }
--- kotlin/src/test/kotlin/stringcalculator/SampleTest.kt
+++ kotlin/src/test/kotlin/stringcalculator/SampleTest.kt
@@ -1,13 +1,21 @@
 package stringcalculator
 
-import org.junit.Assert
-import org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class SampleTest {
 
     @Test
     fun `string calculator with an empty string`() {
-        Assert.assertEquals(0, StringCalculator().add(""))
+        assertEquals(0, StringCalculator().add(""))
     }
+
+    @Test
+    fun `string calculator with one number`() {
+        val result = StringCalculator().add("1")
+
+        assertEquals(1, result)
+    }
+
+    
 }
